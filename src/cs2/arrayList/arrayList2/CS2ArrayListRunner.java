bbdfa77a7c888a0bc75849a8da7b25965851bb23
Mason Rein <marein01@bvsd.org>
2019-08-29 15:08:24 -0600

    CS2 Iterator Started

---
+++ src/cs2/arrayList/arrayList2/CS2ArrayListRunner.java
@@ -0,0 +1,33 @@
+package cs2.arrayList.arrayList2;
+
+import java.util.Iterator;
+
+public class CS2ArrayListRunner {
+    public static void main(String[] args) {
+        CS2ArrayList<Double> list= new CS2ArrayList<Double>();
+
+        // test iterator
+
+        for (int i=0; i<20; i++) {
+            list.add (Math.random()-0.5);
+        }
+        Iterator<Double> it = list.iterator();
+        while (it.hasNext()) {
+            Double val = it.next();
+            System.out.println (val + " ");
+        }
+        System.out.println ();
+        for (Double val: list) {
+            System.out.println (val + " ");
+        }
+        System.out.println ();
+
+        System.out.println (list);
+        it = list.iterator();
+        while (it.hasNext()) {
+            if (it.next() < 0.0) it.remove();
+        }
+        System.out.println (list);
+
+    }
+}

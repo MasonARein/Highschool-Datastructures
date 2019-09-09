0a68263e31a173bce5624494d70aedf6a11347fc
Mason Rein <marein01@bvsd.org>
2019-09-09 14:19:23 -0600

    CS2 LinkedList Started

---
+++ src/cs2/linkedList/CS2LinkedList.java
@@ -0,0 +1,61 @@
+package cs2.linkedList;
+
+import cs2.CS2List;
+
+public class CS2LinkedList<E> implements CS2List<E> {
+
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean add(E obj) {
+        return false;
+    }
+
+    @Override
+    public void add(int index, E obj) {
+
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public E set(int index, E obj) {
+        return null;
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+    private class ListNode{
+        private E value;
+        private ListNode next;
+
+        public ListNode(E item){
+            next = null;
+            value = item;
+        }
+
+        public E getValue() {
+            return value;
+        }
+
+        public void setValue(E value) {
+            this.value = value;
+        }
+        public ListNode getNext() {
+            return next;
+        }
+
+        public void setNext(ListNode next) {
+            this.next = next;
+        }
+    }
+}

cfcb303cf3be8c9ced1b4d1ee5096093592ff073
Mason Rein <marein01@bvsd.org>
2019-10-08 14:54:05 -0600

    Maze Done

--- src/cs2/maze/Mouse.java
+++ src/cs2/maze/Mouse.java
@@ -1,36 +1,82 @@
 package cs2.maze;
 
+import cs2.stack.CS2Stack;
 import info.gridworld.actor.Actor;
 import info.gridworld.grid.Location;
+import jdk.nashorn.internal.parser.JSONParser;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class Mouse extends Actor {
+    private CS2Stack<Location> all = new CS2Stack<Location>();
+    private CS2Stack<Location> allBread = new CS2Stack<Location>();
+    private boolean stop;
     public Mouse(){
         this.setColor(null);
-        if(findCheese() != null){
-            this.getGrid().get(findCheese()).removeSelfFromGrid();
-            this.moveTo(findCheese());
-        }
+        stop = false;
     }
-    private Location findCheese(){
-        for(int a = 0; a < 4; a++){
-            if(getLocation().getAdjacentLocation(a * 90) != null || this.getGrid().get(getLocation().getAdjacentLocation(a * 90)) instanceof Cheese){
-                return getLocation().getAdjacentLocation(a * 90);
+    private Location findCheese() {
+        ArrayList<Location> locList = new ArrayList<Location>();
+        locList.add(this.getLocation().getAdjacentLocation(Location.NORTH));
+        locList.add(this.getLocation().getAdjacentLocation(Location.EAST));
+        locList.add(this.getLocation().getAdjacentLocation(Location.SOUTH));
+        locList.add(this.getLocation().getAdjacentLocation(Location.WEST));
+        for (Location l : locList) {
+            if (this.getGrid().isValid(l) && this.getGrid().get(l) instanceof Cheese) {
+                return l;
             }
         }
         return null;
     }
-    public ArrayList<Location> getLocations(){
-        ArrayList<Location> ava = new ArrayList<Location>();
-        for(int b = 0; b < 4; b++){
-            if(this.getGrid().get(getLocation().getAdjacentLocation(b * 90)) == null){
-                ava.add(getLocation().getAdjacentLocation(b * 90));
+
+    private ArrayList<Location> getLocs(){
+        ArrayList<Location> locList = new ArrayList<Location>();
+        ArrayList<Location> retList = new ArrayList<Location>();
+        locList.add(this.getLocation().getAdjacentLocation(Location.NORTH));
+        locList.add(this.getLocation().getAdjacentLocation(Location.EAST));
+        locList.add(this.getLocation().getAdjacentLocation(Location.SOUTH));
+        locList.add(this.getLocation().getAdjacentLocation(Location.WEST));
+        for (Location l : locList) {
+            if (this.getGrid().isValid(l) && this.getGrid().get(l)==null) {
+                retList.add(l);
             }
         }
-        return ava;
+        return retList;
     }
+
     public void act(){
+        if(!stop) {
+            if (findCheese() != null) {
+                Location cur = getLocation();
+                Breadcrumb a = new Breadcrumb();
+                Location temp = findCheese();
+                this.getGrid().get(findCheese()).removeSelfFromGrid();
+                this.moveTo(temp);
+                a.putSelfInGrid(getGrid(), cur);
+                stop = true;
+            } else {
+                ArrayList<Location> ex = this.getLocs();
+                System.out.println(all);
+                for (Location x : ex) {
+                    all.push(x);
+                }
+                Location current = this.getLocation();
+                Location moveTo;
+                if (ex.size() != 0) {
+                    moveTo = all.pop();
+                } else {
+                    moveTo = allBread.pop();
+                }
+                this.setDirection(getLocation().getDirectionToward(moveTo));
+                this.moveTo(moveTo);
+                Breadcrumb b = new Breadcrumb();
+                b.putSelfInGrid(getGrid(), current);
+                if (ex.size() != 0) {
+                    allBread.push(b.getLocation());
+                }
+            }
+        }
 
     }
 }

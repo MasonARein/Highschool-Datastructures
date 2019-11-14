c5a9c24e4dcccd9050cbacfaf3bab4fac711815f
Mason Rein <marein01@bvsd.org>
2019-11-14 15:09:23 -0700

    Heap half

--- src/cs2/heap/Heap.java
+++ src/cs2/heap/Heap.java
@@ -1,17 +1,46 @@
 package cs2.heap;
 
-import java.util.ArrayList;
+import java.util.*;
 
-public class Heap <E extends Comparable<E>> {
+public class Heap <E extends Comparable<E>> implements Iterable {
     private ArrayList<E> vals = new ArrayList<>();
 
     public void add(E val){
         vals.add(val);
-        while(val.compareTo(vals.get(parent(size()-1))) < 0){
-            int stor = size()-1;
-            E item = val;
-            vals.set(stor, vals.get(parent(stor)));
-            vals.set(parent(stor), item);
+        int index = size()-1;
+        while(val.compareTo(vals.get(parent(index)))<0) {
+            vals.set(index, vals.get(parent(index)));
+            vals.set(parent(index), val);
+            val = vals.get(parent(index));
+            index = parent(index);
+        }
+    }
+    public E remove() {
+        E ite = vals.get(0);
+        vals.set(0, vals.get(size()-1));
+        vals.remove(size()-1);
+        for(int a = 0; a < size(); a++){
+            swap(a);
+        }
+        return ite;
+    }
+    public void swap(int a){
+        try {
+            if (vals.get(leftChild(a)).compareTo(vals.get(rightChild(a))) > 0) {
+                if (vals.get(a).compareTo(vals.get(rightChild(a))) > 0) {
+                    E temp = vals.get(a);
+                    vals.set(a, vals.get(rightChild(a)));
+                    vals.set(rightChild(a), temp);
+                }
+            } else {
+                if (vals.get(a).compareTo(vals.get(leftChild(a))) > 0) {
+                    E temp = vals.get(a);
+                    vals.set(a, vals.get(leftChild(a)));
+                    vals.set(leftChild(a), temp);
+                }
+            }
+        }
+        catch (Exception e){
 
         }
     }
@@ -61,5 +90,41 @@ public class Heap <E extends Comparable<E>> {
     }
     public boolean isEmpty(){
         return vals.isEmpty();
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new CS2HeapIterator();
+    }
+    private class CS2HeapIterator implements Iterator<E>{
+        private Iterator<E> iter;
+        private List content;
+
+
+        public CS2HeapIterator(){
+            content = new ArrayList();
+            Queue<E> ite = new LinkedList<>();
+            while(!vals.isEmpty()){
+                ite.add(Heap.this.remove());
+            }
+            while(!ite.isEmpty()){
+                E item = ite.remove();
+                content.add(item);
+                vals.add(item);
+            }
+            iter = content.iterator();
+
+
+        }
+        public boolean hasNext(){
+            return iter.hasNext();
+        }
+        public E next(){
+            return iter.next();
+        }
+        public void remove(){
+            throw new UnsupportedOperationException("Remove not Known");
+        }
+
     }
 }
